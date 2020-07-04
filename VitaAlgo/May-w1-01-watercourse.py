5c35078c91989c1eb6d678a4e76277c62ac7cf36
gitgitWi <johnwi@knou.ac.kr>
2020-07-04 04:42:43 +0000

    rename some files, add LV2 쇠막대기

---
+++ VitaAlgo/May-w1-01-watercourse.py
@@ -0,0 +1,11 @@
+'''
+https://edu.goorm.io/learn/lecture/18444/시즌3-연재-중-위클리-비타알고-중급편/lesson/966430/5월-1주차-수로-3
+
+mkdate : 2020-07-04
+
+
+'''
+
+n = int (input())
+
+courses = [list(map(int, input().split())) for _ in range(4)]
