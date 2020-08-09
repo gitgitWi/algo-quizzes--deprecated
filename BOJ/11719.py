8248b056e6cd03fd3af5925a23665e55b2513961
gitgitwi <johnwi@knou.ac.kr>
2020-08-09 22:14:27 +0900

    11719 그대로 출력하기 2

---
+++ BOJ/11719.py
@@ -0,0 +1,23 @@
+import sys
+
+output = ""
+for w in sys.stdin: 
+    # print (w)
+    output += w
+print (output)
+
+
+'''
+
+# Short Codings
+
+# https://www.acmicpc.net/source/7503700
+
+import sys
+print(sys.stdin.read())
+
+# https://www.acmicpc.net/source/18345625
+
+print(open(0).read())
+
+'''
