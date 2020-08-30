2b1ed4e569a25a0fcfd1bef8a222a5617f0376b1
gitgitWi <johnwi@knou.ac.kr>
2020-08-30 16:18:00 +0000

    add prgm 42746

--- MUST_REVIEW/Programmers/lv2-42746.py
+++ MUST_REVIEW/Programmers/lv2-42746.py
@@ -13,7 +13,7 @@ https://programmers.co.kr/learn/courses/30/lessons/42746
 다른 풀이들을 보니, QuickSort, sort/sorted 함수의 key에서 tuple 을 이용한 경우 등
 근접하게 생각은 했는데, 적용을 계속 엉뚱하게 해서 몇시간을 헤매고 있었다.
 
-뭔가 가장 pythonic 한 답은 string 값 비교 시 앞에서부터 한 unicode 단위로 비교하는 걸 활용한 답인 것 같아서 
+뭔가 가장 pythonic 한 답은 string 값 비교 시 앞에서부터 한 ascii 단위로 비교하는 걸 활용한 답인 것 같아서 
 일단 이걸로 제출했음..
 
 refs)
