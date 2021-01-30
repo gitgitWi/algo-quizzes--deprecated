3b8c60c5e00fb4930ef555111a53d6977ab0d039
gitgitwi <johnwi@knou.ac.kr>
2021-01-30 18:03:45 +0900

    [SOLVED/update] LeetCode/Medium/39.CombinationSum(백트래킹)
    - 개선된 코드 추가

--- SOLVED/leetcode/39.combination-sum.js
+++ SOLVED/leetcode/39.combination-sum.js
@@ -5,7 +5,11 @@
  */
 
 // @lc code=start
-
+/**
+ *  1차 제출
+ *
+ *  모든 경우의 수를 구한 뒤, 중복은 sort, map 활용해 제거
+ */
 const getSum = (candidates, currentTarget, ans, sum = []) => {
 	if (currentTarget === 0) {
 		const sortedSum = sum.sort((a, b) => a - b);
@@ -29,5 +33,33 @@ const combinationSum = (candidates, target) => {
 	const ans = {};
 	getSum(candidates, target, ans);
 	return Object.values(ans);
+};
+
+/**
+ *  개선된 방법
+ *
+ *  현재 인덱스 이상인 경우에만 재귀 호출
+ *
+ *  참고
+ *  - 코드없는 프로그래밍
+ *  - Combination 합 (https://youtu.be/C6vZH6hnzJg)
+ */
+const getSum = (candidates, currentTarget, idx, ans, sum = []) => {
+	if (currentTarget === 0) {
+		const sortedSum = sum.sort((a, b) => a - b);
+		ans.push(sortedSum);
+		return;
+	}
+
+	candidates.forEach((can, i) => {
+		if (currentTarget >= can && i >= idx)
+			getSum(candidates, currentTarget - can, i, ans, sum.concat(can));
+	});
+};
+
+const combinationSum = (candidates, target) => {
+	const ans = [];
+	getSum(candidates, target, 0, ans);
+	return ans;
 };
 // @lc code=end
