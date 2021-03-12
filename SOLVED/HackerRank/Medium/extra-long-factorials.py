071526acd67f41e9dd2633a6955c2e2608124ef9
NotePenS <57997672+ezerwi@users.noreply.github.com>
2021-03-12 17:10:54 +0900

    [Solved] HackerRank/Medium/Extra Long Factorials

---
+++ SOLVED/HackerRank/Medium/extra-long-factorials.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    ans =1;
+    for i in range(1, n+1):
+        ans *= i
+    print (ans)
+
+if __name__ == '__main__':
+    n = int(input())
+    extraLongFactorials(n)
